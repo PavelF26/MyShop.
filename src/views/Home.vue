90b8abfd4ed6188b41e23004fd5b81ff8101a90c
Pavel Filip <pavelfilip@Pavel-MacBook-Pro.local>
2022-01-16 17:37:14 +0100

    add img iphone_13

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="header-img">
+      <div class="basic-slider ptb-200">
+        <div class="container">
+          <div class="hero-caption pt-30">
+            <div class="slider-content hero-text">
+              
+              <a class="btn">Shop</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <v-col>
+   
+    </v-col>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+  data: () => ({})
 };
 </script>
+
+<style scoped>
+.header-img {
+  background-image: url("../assets/img/hero_iphone_13pro.jpeg");
+  height: 664px;
+  width: 100% !important;
+  min-width: 100% !important;
+  max-width: 100% !important;
+  background-size: cover;
+  background-position: center;
+}
+.hero-caption {
+  display: table;
+  height: 100%;
+  margin: 0 auto;
+  position: relative;
+  width: 80%;
+  z-index: 2;
+}
+.hero-text {
+  display: table-cell;
+  height: 100%;
+  position: relative;
+  vertical-align: middle;
+}
+.basic-slider {
+  position: relative;
+}
+.slider-content h2 {
+  font-size: 42px;
+  font-weight: 700;
+  letter-spacing: 2px;
+  margin: 0;
+  margin-bottom: 20px;
+}
+</style>
