ad453bfe2601c349f67bb873f32b7e0bdbf5384b
Pavel Filip <Pavel.filip26@email.cz>
2022-01-28 15:30:19 +0100

    I added store + catalog. these sites are clean.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,7 @@
     </div>
 
     </div>
+    
     <v-col>
    
     </v-col>
@@ -37,8 +38,6 @@ export default {
   background-size: cover;
   background-position: center;
 }
-
-
 
 
 .hero-caption {
