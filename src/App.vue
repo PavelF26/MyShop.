d6e209ca7c8c5990c9e6d2cff71c0cd20c72b98e
Pavel Filip <Pavel.filip26@email.cz>
2022-01-30 19:40:54 +0100

    add scroll

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="nav">
     <Navbar/>
-  </div>
   <router-view />
+  <ScrollTopArrow/>
+  
   <Footer />
+  </div>
 </template>
 
 
 <script>
 import Footer from './components/Footer.vue';
 import Navbar from './components/Navbar.vue';
+import ScrollTopArrow from './components/ScrollTopArrow.vue';
+
 
 
 
 export default {
-  components: { Footer, Navbar }
+  components: { Footer, Navbar,ScrollTopArrow }
   
 }
 </script>
@@ -23,4 +27,16 @@ export default {
 @import "./assets/styles/style.css";
 @import "./assets/styles/global.css";
 @import "./assets/styles/colors.css";
+
+.btn-up {
+    border-radius: 8px;
+    background-color: rgba(0, 0, 0, 0.55);
+    padding-top: 27px;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-bottom: 5px;
+}
+
+
 </style>
+
