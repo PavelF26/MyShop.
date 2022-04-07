b104f6290aa667d71fb9a96a7eababd7c5632f83
Pavel Filip <Pavel.filip26@email.cz>
2022-04-07 16:09:47 +0200

    next

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,6 +23,82 @@
       <img class="header-img05" src="" alt="">
       
     </div>
+
+    <section class="product">
+        <h2 class="product-category">Best selling</h2>
+
+               <button class="pre-btn"><img src="../assets/img/arrow.png" alt=""></button>
+                <button class="nxt-btn"><img src="../assets/img/arrow.png" alt=""></button>
+
+              <div class="product-container">
+                  <div class="product-card">
+                    <div class="product-image">
+                            <img src="../assets/img/macbookpro.jpg" class="product-thumb" alt="" >
+                            <button class="card-btn">add to wishlist </button>
+                    </div>
+                        <h2 class="product-info"> brand</h2>
+                        <p class="product-short-description"> a short line about the product</p>
+                        <span class="price">$20</span>
+                        <span class="actual-price">$40</span>
+                  </div>
+
+                   <div class="product-card">
+                    <div class="product-image">
+                            <img src="../assets/img/macbookpro.jpg" class="product-thumb" alt="" >
+                            <button class="card-btn">add to wishlist </button>
+                    </div>
+                        <h2 class="product-info"> brand</h2>
+                        <p class="product-short-description"> a short line about the product</p>
+                        <span class="price">$20</span>
+                        <span class="actual-price">$40</span>
+                  </div>
+
+                   <div class="product-card">
+                    <div class="product-image">
+                            <img src="../assets/img/macbookpro.jpg" class="product-thumb" alt="" >
+                            <button class="card-btn">add to wishlist </button>
+                    </div>
+                        <h2 class="product-info"> brand</h2>
+                        <p class="product-short-description"> a short line about the product</p>
+                        <span class="price">$20</span>
+                        <span class="actual-price">$40</span>
+                  </div>
+
+                   <div class="product-card">
+                    <div class="product-image">
+                            <img src="../assets/img/macbookpro.jpg" class="product-thumb" alt="" >
+                            <button class="card-btn">add to wishlist </button>
+                    </div>
+                        <h2 class="product-info"> brand</h2>
+                        <p class="product-short-description"> a short line about the product</p>
+                        <span class="price">$20</span>
+                        <span class="actual-price">$40</span>
+                  </div>
+
+                   <div class="product-card">
+                    <div class="product-image">
+                            <img src="../assets/img/macbookpro.jpg" class="product-thumb" alt="" >
+                            <button class="card-btn">add to wishlist </button>
+                    </div>
+                        <h2 class="product-info"> brand</h2>
+                        <p class="product-short-description"> a short line about the product</p>
+                        <span class="price">$20</span>
+                        <span class="actual-price">$40</span>
+                  </div>
+
+                   <div class="product-card">
+                    <div class="product-image">
+                            <img src="../assets/img/macbookpro.jpg" class="product-thumb" alt="" >
+                            <button class="card-btn">add to wishlist </button>
+                    </div>
+                        <h2 class="product-info"> brand</h2>
+                        <p class="product-short-description"> a short line about the product</p>
+                        <span class="price">$20</span>
+                        <span class="actual-price">$40</span>
+                  </div>
+              </div>
+
+    </section>
     
     <v-col>
    
@@ -32,6 +108,28 @@
 </template>
 
 <script>
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+
+
+
+
+
 export default {
   data: () => ({})
 };
@@ -119,4 +217,188 @@ text-align: center;
   margin: 0;
   margin-bottom: 20px;
 }
+
+
+/* PRODUCTS SLIDER */
+  .product {
+  position: relative;
+  overflow: hidden;
+  padding: 20px;
+}
+
+.product-category {
+  padding: 0 10vw;
+  font-size: 30px;
+  font-weight: 500;
+  margin-bottom: 40px;
+  text-transform: capitalize;
+}
+
+.product-container {
+  padding: 0 10vw;
+  display: flex;
+  overflow-x: auto;
+  scroll-behavior: smooth;
+}
+
+.product-container::-webkit-scrollbar {
+  display: none;
+}
+
+.product-card {
+  flex: 0 0 auto;
+  width: 250px;
+  height: 450px;
+  margin-right: 40px;
+}
+
+.product-image {
+  position: relative;
+  width: 100%;
+  height: 350px;
+  overflow: hidden;
+}
+
+.product-thumb {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.discount-tag {
+  position: absolute;
+  background: #fff;
+  padding: 5px;
+  border-radius: 5px;
+  color: silver;
+  right: 10px;
+  top: 10px;
+  text-transform: capitalize;
+}
+
+.card-btn {
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 10px;
+  width: 90%;
+  text-transform: capitalize;
+  border: none;
+  outline: none;
+  background: #fff;
+  border-radius: 5px;
+  transition: 0.5s;
+  cursor: pointer;
+  opacity: 0;
+}
+
+.product-card:hover .card-btn {
+  opacity: 1;
+}
+
+.card-btn:hover {
+  background: silver;
+  color: #fff;
+}
+
+.product-info {
+  width: 100%;
+  height: 100px;
+  padding-top: 10px;
+}
+
+.product-brand {
+  text-transform: uppercase;
+}
+
+.product-short-description {
+  width: 100%;
+  height: 20px;
+  line-height: 20px;
+  overflow: hidden;
+  opacity: 0.5;
+  text-transform: capitalize;
+  margin: 5px 0;
+}
+
+.price {
+  font-weight: 900;
+  font-size: 20px;
+}
+
+.actual-price {
+  margin-left: 20px;
+  opacity: 0.5;
+  text-decoration: line-through;
+}
+
+.pre-btn,
+.nxt-btn {
+  border: none;
+  width: 10vw;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, #fff 100%);
+  cursor: pointer;
+  z-index: 8;
+}
+
+.pre-btn {
+  left: 0;
+  transform: rotate(180deg);
+}
+
+.nxt-btn {
+  right: 0;
+}
+
+.pre-btn img,
+.nxt-btn img {
+  opacity: 0.2;
+}
+
+.pre-btn:hover img,
+.nxt-btn:hover img {
+  opacity: 1;
+}
+
+.collection-container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+}
+
+.collection {
+  position: relative;
+}
+
+.collection img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.collection p {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: #fff;
+  font-size: 50px;
+  text-transform: capitalize;
+}
+
+.collection:nth-child(3) {
+  grid-column: span 2;
+  margin-bottom: 10px;
+}
+
+
 </style>
